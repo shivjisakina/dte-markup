8b5a115df84abba5156a9dd379b1fb0bcf161ccf
sakinashivji <sakina.shivji@perficient.com>
2019-11-22 18:10:47 -0500

    updated mobile

--- script.js
+++ script.js
@@ -12,15 +12,32 @@ function toggleShowMore()
   }
 }
 
-var toggle = false;
-function toggleMinus() {
-    if (toggle === false) {
-        document.getElementById('plus').src  = 'resources/images/plus.svg';
-    } else {
-       document.getElementById('plus').src = 'resources/images/minus.png';
-       document.getElementById("accordion-header").classList.add('header-active');
-       alert(img); 
-    }
-    toggle = !toggle; 
-}
-
+// var toggle = document.getElementsByClassName('btn-link');
+// console.log(toggle)
+// function toggleMinus() {
+//     if (toggle === false) {
+//         console.log(toggle)
+//         document.getElementById('plus').src  = 'resources/images/plus.svg';
+//     } else {
+//         console.log(toggle)
+//        document.getElementById('plus').src = 'resources/images/minus.png';
+//        document.getElementById("accordion-button").classList.add('header-active');
+//        alert(img); 
+//     }
+//     toggle = !toggle; 
+// }
+$( document ).ready(function() {
+ $('.btn-link').on('click', function(){
+     var $this = $(this);
+    console.log( $this.text());
+     if($this.parent().siblings().hasClass('plus')){
+        $this.parent().parent().parent().addClass('header-active');
+    $this.parent().siblings('.plus').attr('src','resources/images/minus.png');
+    $this.parent().siblings('.plus').addClass('minus');
+     }else{
+         console.log($this.parent().parent().parent())
+        $this.parent().parent().parent().removeClass('header-active');
+        $this.parent().siblings('.plus').attr('src','resources/images/plus.svg');
+     }
+ });
+});

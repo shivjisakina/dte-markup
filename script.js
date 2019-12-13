f837771fbea222d7fdc47e8950f08d63f1f59897
sakinashivji <sakina.shivji@perficient.com>
2019-12-13 00:34:54 -0500

    updated carousel and faq

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-function toggleShowMore()
-{
-    var x = document.getElementById("showMoreDiv");
+function toggleShowMore() {
+  var x = document.getElementById("showMoreDiv");
   if (x.style.display === "none") {
     x.style.display = "block";
     document.getElementById("toggleButton").innerHTML = "Show less"
@@ -10,9 +8,9 @@ function toggleShowMore()
     document.getElementById("toggleButton").innerHTML = "Show More"
   }
 }
-function toggleTerms()
-{
-    var x = document.getElementById("terms-div");
+
+function toggleTerms() {
+  var x = document.getElementById("terms-div");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -20,60 +18,46 @@ function toggleTerms()
   }
 }
 
-// var toggle = document.getElementsByClassName('btn-link');
-// console.log(toggle)
-// function toggleMinus() {
-//     if (toggle === false) {
-//         console.log(toggle)
-//         document.getElementById('plus').src  = 'resources/images/plus.svg';
-//     } else {
-//         console.log(toggle)
-//        document.getElementById('plus').src = 'resources/images/minus.png';
-//        document.getElementById("accordion-button").classList.add('header-active');
-//        alert(img); 
-//     }
-//     toggle = !toggle; 
-// }
- $( document ).ready(function() {
-/*  $('.accept-terms button').attr("disabled", true);
-  $('.accept-terms .checkmark').click(function() {
-    console.log($('.accept-terms .btn'))
-    $('.accept-terms .btn').removeAttr('disabled');
-  if ($(this).is(':checked')) {
-  } else {
-  $('.accept-terms button').attr('disabled',true);}
-  }); */
+$(document).ready(function () {
 
-  $(".accept-terms .checkmark").click(function(){
+  $(".accept-terms .checkmark").click(function () {
     console.log($('.accept-terms .btn'))
     $('.accept-terms .btn').prop("disabled", false);
     $(".accept-terms:input").prop("checked")
     if ($(this).is(':checked')) {
       $('.accept-terms .btn').prop("disabled", false);
-      } else {
-      $('.accept-terms .btn').attr('disabled',true);}
- });
+    } else {
+      $('.accept-terms .btn').attr('disabled', true);
+    }
+  });
 
- 
- $('.card').click(function(){
-     var $this = $(this);
-     console.log($this.children().children().children())
-     if($this.children().hasClass('collapse')){
-       console.log("hi")
-       $this.children('.card-header').addClass('header-active');
-       $this.children('.card-header').children(".icon-div").children().attr('src','resources/images/minus.png');
-       $this.children('.card-header').children(".icon-div").children().addClass('minus');
-       $this.children('.card-header').children(".icon-div").children().removeClass('plus');
-       $this.children().removeClass('collapse')
-     }else if (!$this.children().hasClass('collapse')){
-      console.log($this.children().children().children())
+  $('.card').click(function () {
+    var $this = $(this);
+    console.log("first" + ($this.children().children().children()))
+    if (!$this.children().hasClass('show')) {
+      console.log("hi")
+      $this.children('.card-header').addClass('header-active');
+      $this.children('.card-header').children(".icon-div").children().attr('src', 'resources/images/minus.png');
+      $this.children('.card-header').children(".icon-div").children().addClass('minus');
+      $this.children('.card-header').children(".icon-div").children().removeClass('plus');
+      $this.children().removeClass('collapse')
+    } else if ($this.children().hasClass('show')) {
+      console.log("second" + ($this.children().children().children()))
       console.log("hello")
       $this.children('.card-header').removeClass('header-active');
       $this.children('.card-header').children(".icon-div").children().addClass('plus');
-      $this.children('.card-header').children(".icon-div").children().attr('src','resources/images/plus.svg');
-     }
- });
+      $this.children('.card-header').children(".icon-div").children().attr('src', 'resources/images/plus.svg');
+    }
+  });
+
+  $('.video').parent().click(function () {
+    if($(this).children(".video").get(0).paused){        
+      $(this).children(".video").get(0).play();   
+      $(this).children(".playpause").hide();
+      }else{       
+        $(this).children(".video").get(0).pause();
+        $(this).children(".playpause").hide();
+      }
+  });
 
 });
-
-
